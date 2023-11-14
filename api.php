ea5f488b5b735b16dacf15efcb0bacd92b6f4641
tincke10 <martinmoreiralmq@gmail.com>
2023-11-14 12:29:25 -0300

    Change sintaxis into application and routes

--- api.php
+++ api.php
@@ -1,34 +1,33 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
 require 'vendor/autoload.php';
 
 function validarEmail($email) {
-    // Primero, validar la sintaxis del correo electrónico
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         return false;
     }
 
-    // Luego, realizar la verificación MX
     list($user, $domain) = explode('@', $email);
     return checkdnsrr($domain, 'MX');
 }
 
-$app = new \Slim\App;
+$app = AppFactory::create();
 
 $app->post('/validarCorreo', function (Request $request, Response $response) {
     $data = $request->getParsedBody();
     $email = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
 
-    // Validar el correo electrónico
     if (validarEmail($email)) {
         $result = array('email' => $email, 'valid' => true);
     } else {
         $result = array('email' => $email, 'valid' => false);
     }
 
-    return $response->withJson($result);
+    $response->getBody()->write(json_encode($result));
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
